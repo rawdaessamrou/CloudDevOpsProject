0dc653a5a21f2c56930d20d7d06d6a88b2ba5fdc
Rawda Essam <159459253+rawdaessamrou@users.noreply.github.com>
2026-05-09 03:24:35 +0300

    Add cleanup step before cloning repository

--- vars/manifestUtils.groovy
+++ vars/manifestUtils.groovy
@@ -1,5 +1,6 @@
 def updateManifest(String repo, String imageName, String imageTag) {
     sh """
+        rm -rf CloudDevOpsProject
         git clone ${repo}
         cd CloudDevOpsProject/Kubernates
         sed -i 's|image:.*|image: ${imageName}:${imageTag}|g' deployment.yaml
