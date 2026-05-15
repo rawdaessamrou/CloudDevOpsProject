6a313f8502e8e9e076e1b357d9bb0e99d122a0c2
Rawda Essam <159459253+rawdaessamrou@users.noreply.github.com>
2026-05-15 03:44:48 +0300

    Correct path to deployment.yaml in updateManifest
    Fix typo in the path to the deployment.yaml file.

--- vars/manifestUtils.groovy
+++ vars/manifestUtils.groovy
@@ -2,6 +2,6 @@ def updateManifest(String repo, String imageName, String imageTag) {
     sh """
         git clone ${repo} CloudDevOpsProject
         cd CloudDevOpsProject
-        sed -i 's|image:.*|image: ${imageName}:${imageTag}|g' kubernetes/deployment.yaml
+        sed -i 's|image:.*|image: ${imageName}:${imageTag}|g' kubernates/deployment.yaml
     """
 }
