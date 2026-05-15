58d034ef6c4a830d5882c5b80229896361dc0359
Rawda Essam <159459253+rawdaessamrou@users.noreply.github.com>
2026-05-15 03:49:54 +0300

    Fix typo in path to deployment.yaml

--- vars/manifestUtils.groovy
+++ vars/manifestUtils.groovy
@@ -1,7 +1,9 @@
 def updateManifest(String repo, String imageName, String imageTag) {
     sh """
+        rm -rf CloudDevOpsProject || true
         git clone ${repo} CloudDevOpsProject
         cd CloudDevOpsProject
-        sed -i 's|image:.*|image: ${imageName}:${imageTag}|g' kubernates/deployment.yaml
+
+        sed -i 's|image:.*|image: ${imageName}:${imageTag}|g' Kubernates/deployment.yaml
     """
 }
